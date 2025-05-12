29c515fa543b38af1c2de777416b9487828f34ed
furious-dna <aldarchez01@gmail.com>
2025-05-11 22:29:27 -0600

    Update bot.py

--- bot.py
+++ bot.py
@@ -5,10 +5,10 @@ TOKEN = "7965345260:AAH5_6ei_kMdSuzezn-cC0PoooQNjES31wU"
 STRIPE_LINK = "https://buy.stripe.com/28o01sgDr44Y8pibII"
 
 async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
-    keyboard = [[InlineKeyboardButton("Pagar suscripción", url=STRIPE_LINK)]]
+    keyboard = [[InlineKeyboardButton("Pagar suscripción / Pay subscription", url=STRIPE_LINK)]]
     reply_markup = InlineKeyboardMarkup(keyboard)
     await update.message.reply_text(
-        "¡Hola! Para acceder al canal exclusivo, primero paga tu suscripción mensual:",
+        "Hi there! to have access to the channel, first you need to pay the monthly subscription:",
         reply_markup=reply_markup
     )
 
